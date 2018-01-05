c12ccf2543446047251fab9ffbd3f4a90332ff51
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-01-05 13:09:23 +0530

    Start Working on it

---
+++ PRIME1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 13:06:30 2018
+
+@author: ashwin
+
+Input
+The first line contains t, the number of test cases (less then or equal to 10). Followed by t lines which contain two numbers m and n (1 <= m <= n <= 1000000000, n-m<=100000) separated by a space.
+
+Output
+For every test case print all prime numbers p such that m <= p <= n, one number per line. Separate the answers for each test case by an empty line.
+
+Example
+Input:
+2
+1 10
+3 5
+
+Output:
+2
+3
+5
+7
+
+3
+5
+"""
+
