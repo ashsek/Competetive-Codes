3ec31ce127881f265b31bfb82ca36492fffef67c
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-03-29 17:26:01 +0530

    tidy numbers complete

--- Code Jam/2017/QualRound/tidynumbers.py
+++ Code Jam/2017/QualRound/tidynumbers.py
@@ -1,14 +1,22 @@
-for i in range(int(input())):
-	n = int(input()) # last number to be counted.
-	if n < 10:
-        print("Case #"+ str(i+1) +": " + str(n))
+#Works only for small inputs ,not for large data set.
+# updated :- Here we basically changed all the leading integers from when it starts
+# deviating from tidy numbers and just subtract 1 and we will get our answer easily.
+for j in range(int(input())):
+    o = int(input()) # last number to be counted.
+    if o < 10:
+        print("Case #"+ str(j+1) +": " + str(o))
     else:
-        j = list(str(n))
-        j2 = j[:]
-        j2.sort()
-        while j != j2 and n > 9:
-            n -= 1
-            j = list(str(n))
-            j2 = j[:]
-            j2.sort()
-        print("Case #"+ str(i+1) +": " + str(n))
+        s = 0
+        n = list(map(int,list(str(o))))
+        for i in range(1,len(n)):
+        	if n[i] < n[s]:
+        		break
+        	elif n[i] > n[s]:
+        		s = i
+        for k in range(s+1,len(n)):
+        	n[k] = 0
+        p = 0
+        for d in n:
+        	p = p*10 + d
+
+        print("Case #"+ str(j+1) +": " + str(p-1))
