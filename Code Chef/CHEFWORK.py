711868bca843f301c31aab0be15d6e27deaa68d5
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-04-06 19:04:46 +0530

    Full score

--- Code Chef/CHEFWORK.py
+++ Code Chef/CHEFWORK.py
@@ -5,4 +5,28 @@ t = list(map(int,input().split())) # type of workers
 # t = 1 -> translator
 # t = 2 -> author
 # t = 3 -> both author / translator 
-print(min(c))
+#print(min(c))
+#c = [4,5,6,1,2,3,4,5,2,2,2]
+#t = [1,1,1,2,2,2,3,3,3,1,1]
+l = []
+c2 = c[:]
+coins = 0
+c2.sort()
+for i in c2:
+    index = c.index(i)
+    if t[index] not in l and t[index] != 3:
+        l.append(t[index])
+        coins += i
+    elif t[index] == 3:
+        if len(l) == 0 or len(l) == 1:
+            coins = i
+            break
+        elif len(l) != 0:
+            if coins > i:
+                coins = i
+                break
+    elif l.sort() == [1,2]:
+        break
+    c.pop(index)
+    t.pop(index)
+print(coins)
