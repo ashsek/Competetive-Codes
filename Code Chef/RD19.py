6c1f2bbc952e19f0bcdb6db89820310961e691b2
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-05-05 07:35:09 +0530

    complete

--- Code Chef/RD19.py
+++ Code Chef/RD19.py
@@ -3,11 +3,43 @@ def gcd(a,b):
         a,b = b%a,a
     return b
 
+# global c
+# def gc(l,i):
+# 	f = True
+# 	l2 = l[:]
+# 	for j in l:
+# 		if j%i == 0:
+# 			continue
+# 		else:
+# 			f = False
+# 			c += 1
+# 			l2.remove(j)
+# 	if f:
+# 		print(c)
+# 	else:
+# 		gc(l2)
+
+
+
 
 for i in range(int(input())):
 	n = int(input())
 	l = list(map(int,input().split()))
-	#we can delete n-1 elements 
+	c = 0
+	l.sort()
+	y = gcd(l[0],l[1])
+
+	for i in range(2,len(l)):
+		y = gcd(y,l[i])
+
+	if y == 1:
+		print(0)
+	else:
+		print(-1)
+
+
+	
+'''	#we can delete n-1 elements 
 	# minimum deletions so that GCD = 1.
 	# or say Impossible
 	d = 0
@@ -25,4 +57,9 @@ for i in range(int(input())):
 			print(1)
 		else:
 			print(-1)
-			
+''' 
+## Dummy Constraints over
+
+
+
+
