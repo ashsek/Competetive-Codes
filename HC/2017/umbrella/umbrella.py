6c584824a2addaf7ec864806f5549c89bf1ab136
Ashwin Sekhari <ashsek@Rajans-MacBook-Air.local>
2018-07-16 18:40:21 +0530

    HC

---
+++ HC/2017/umbrella/umbrella.py
@@ -0,0 +1,10 @@
+#N group of people
+#ith brings Ri m readius
+#M acceptable points (1m b/w them)
+#no. of ways to place uncolliding umbrellas mod 10**6 + 7
+for t in range(int(input())):
+	n,m = list(map(int,input().split()))
+	l = []
+	for j in range(n):
+		l.append(int(input()))
+	print(l)
