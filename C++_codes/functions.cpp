489c2323a7fb6d47e7cb52486ed6d16f771d247e
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-08-18 14:32:41 +0530

    C++ codes

---
+++ C++_codes/functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d) {
+    int m1;
+    int m2;
+    m1 = max(a,b);
+    m2 = max(c,d);
+    return max(m1,m2);
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    return 0;
+}
