cf19e24132244f014692e3c6661b2d817707c0d5
Ashwin Sekhari <ashwinsekhari@gmail.com>
2018-08-18 14:29:44 +0530

    C++ codes

---
+++ C++_codes/input.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n1;
+    int n2;
+    int n3;
+    cin >> n1 >> n2 >> n3;
+    cout << n1 +n2 +n3 << endl;
+    return 0;
+}
+
